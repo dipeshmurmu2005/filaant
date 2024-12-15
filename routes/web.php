78f2b6ff856dd2ffa5b0df97294d272cb92bf204
dipeshmurmu2005 <dipeshmurmu7@gmail.com>
2024-12-15 20:14:22 +0545

    adds essential packages

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
+use App\Livewire\Editor;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', Editor::class)->name('editor');
